8960472c8813363035308e1123b802aa7981f894
vkrasnovyd <v.krasnovyd@gmail.com>
2025-02-12 17:09:47 +0100

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-// write your code here
+const table = document.querySelector('table');
+const rowsArray = Array.from(table.rows);
+const columnsOrder = [0, 1, 2, 3, 1, 4];
+
+rowsArray.forEach((row) => {
+  const cellsArray = Array.from(row.cells);
+  const rowFragment = document.createDocumentFragment();
+
+  columnsOrder.forEach((index) => {
+    rowFragment.appendChild(cellsArray[index].cloneNode(true));
+  });
+
+  row.innerHTML = '';
+  row.appendChild(rowFragment);
+});
