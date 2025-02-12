60509bc45870dc8634937ba29b193ce3a3421650
vkrasnovyd <v.krasnovyd@gmail.com>
2025-02-12 17:17:12 +0100

    Optimize handling of large tables

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,6 +4,11 @@ const table = document.querySelector('table');
 const rowsArray = Array.from(table.rows);
 const columnsOrder = [0, 1, 2, 3, 1, 4];
 
+// Detach table to optimise handling of large tables
+const tableParent = table.parentNode;
+
+tableParent.removeChild(table);
+
 rowsArray.forEach((row) => {
   const cellsArray = Array.from(row.cells);
   const rowFragment = document.createDocumentFragment();
@@ -15,3 +20,6 @@ rowsArray.forEach((row) => {
   row.innerHTML = '';
   row.appendChild(rowFragment);
 });
+
+// Reattach table
+tableParent.appendChild(table);
